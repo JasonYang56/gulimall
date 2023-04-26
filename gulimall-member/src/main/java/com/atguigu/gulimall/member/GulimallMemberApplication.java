70b7c769fb13603db835ce5eafc00676a8e59fc5
Jason <339197878@qq.com>
2023-04-26 11:57:28 +0800

    项目初始提交2

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMemberApplication {
+
+    public static void main(String[] args) {
+//        try{
+            SpringApplication.run(GulimallMemberApplication.class, args);
+//        }catch (RuntimeException e){
+//            System.out.println(e);
+//        }
+
+    }
+
+}
