eef34f1dcb14098ded9e555b04fceda84b5104f7
Jason <339197878@qq.com>
2023-04-27 12:09:53 +0800

    项目初始提交3

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GulimallProductApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GulimallProductApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallProductApplication.class, args);
+	}
 
 }
